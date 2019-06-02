f5a5958d97197274a60d31be29f4fa4487b6572e
onoroff <adam.grunke@gmail.com>
2019-06-01 20:13:18 -0700

    Random word list and reset function

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
 //Variable Declarations
-var wordSolution ='apple';   // empty string at begining. Using "test" during writing.
+var wordSolution ='test';   // empty string at begining. Using "test" during writing.
 var letterGuess;
 var btnRst;
+var btnGuess;
 var dispCorGuess;
 var dispBadGuess;
 var wrongArr = [];
 var corArr = [];
 var wordArr = [];
 var wordDisp = [];
+//var errorCount;
+var rnd;
+
+var wordList = [
+    'apple', 
+    'orange', 
+    'bananas', 
+    'apple', 
+    'orange', 
+    'bananas', 
+    'test',
+    'strawberries',
+    'tacos',
+    'pizza'];
 
 // HTML Element References
 btnRst = document.getElementById("reset");
@@ -19,10 +34,28 @@ dispBadGuess = document.getElementById('wrong');
 
 //Any additional functions
 
-wordArr = wordSolution.toUpperCase().split('');
-//wordDisp = wordArr;
-for (let i = 0; i < wordArr.length; i++){
-    wordDisp[i] = '*';
+var reset = function(){
+    corArr = [];
+    wrongArr = [];
+    wordDisp = [];
+    //errorCount = 0;
+    letterGuess.value = '';
+    dispCorGuess.textContent = "Correct: " + corArr.join(', ');
+    dispBadGuess.textContent = "Wrong: " + wrongArr.join(', ');
+    dispSolution.textContent = "Game: " + wordDisp.join(' ');
+
+
+    getWord();
+} 
+
+var getWord = function(){
+    // create random value between 0 and 9 to select from wordList array.
+    rnd = Math.floor(Math.random()*10);
+    wordSolution = wordList[rnd].toUpperCase();
+    wordArr = wordSolution.split(''); 
+    for (let i = 0; i < wordArr.length; i++){
+        wordDisp[i] = '*';
+    }
 }
 
 var checkSingleLetter = function(length){
@@ -35,14 +68,10 @@ var checkSingleLetter = function(length){
     }
 };
 
-
 var displayLetters = function(letter){ 
     for (let i = 0; i < wordDisp.length; i++){
         if (wordArr[i] === letter) {
             wordDisp[i] = letter;
-        }
-        else {
-            console.log("should never get this message!")
         }
     }
     dispSolution.textContent = "Game: " + wordDisp.join(' ');
@@ -50,25 +79,47 @@ var displayLetters = function(letter){
 }
 
 var guessCompare = function(letter){
+    //console.log("guessCompare.."); //DELETE
     if (wordSolution.includes(letter)){
+        //console.log("gsComp if..") //DELETE
         corArr.push(letter);
         dispCorGuess.textContent = "Correct: " + corArr.join(', ');
         displayLetters(letter);
         
     }
     else {
+       //console.log("gsComp else.."); //DELETE
         wrongArr.push(letter);
         dispBadGuess.textContent = "Wrong: " + wrongArr.join(', ');
     }
 };
-
-btnGuess.addEventListener("click", function(){
+var newTextValue = function(){
+    //console.log("newTextValue .."); //DELETE
     if (checkSingleLetter(letterGuess.value.length)){
         guessCompare(letterGuess.value.toUpperCase());
     };
     letterGuess.value = '';
+};
+
+var init = function(){
+    reset();
+};
+
+init();
+// ********************************************
+
+btnGuess.addEventListener("click", newTextValue);
+
+//https://stackoverflow.com/questions/14542062/eventlistener-enter-key
+// I found this solution for using carriage return to enter a value.
+letterGuess.addEventListener('keypress', function(e){
+    let key = e.which || e.keyCode;
+    if (key === 13) { 
+        newTextValue();
+    }
 });
 
-wordSolution = wordSolution.toUpperCase();
+btnRst.addEventListener("click", reset);
+
 
 
